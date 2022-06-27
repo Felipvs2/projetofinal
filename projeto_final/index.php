1197564dc8b3b12189c31a219b76b4e4088eda46
Felipe  Alves <13010105614@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-27 09:32:52 -0300

    Front-controller

---
+++ projeto_final/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    if(isset($_GET['c'])){
+        $controller = ucfirst($_GET['c']);
+        $path_controller = "controller/$controller.php";
+
+        //verifica se o arquivo de controller existe
+        if(file_exists($path_controller)){
+            require $path_controller;
+
+            //verifica se foi enviada a variável
+            //que contém o método do cotrolador que desejo executar
+            
+                $metodo = $_GET['m'] ?? "index";
+
+                //cria o objeto controlador
+                $obj = new $controller();
+
+                //verifica se o controlador possui uma função
+                if(is_callable(array($obj, $metodo))){
+                    // sei lá tlgd
+
+                    call_user_func_array(array($obj,$metodo),array());
+                }
+            
+           
+        }
+
+    }
+
+
+?>
